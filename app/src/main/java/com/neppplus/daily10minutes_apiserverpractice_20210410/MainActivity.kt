744ddab0297255a7ac7697f46b45fe0c7972b3b0
전은형 <ooooo5h91@gmail.com>
2021-04-10 11:19:58 +0900

    입력한 이메일/비번 추출까지

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/MainActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.daily10minutes_apiserverpractice_20210410
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +14,18 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+
+//            입력한 이메일/비번 추출
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPassword = passwordEdt.text.toString()
+
+
+//            서버에 로그인 요청(배울 내용)
+
+        }
 
     }
 
