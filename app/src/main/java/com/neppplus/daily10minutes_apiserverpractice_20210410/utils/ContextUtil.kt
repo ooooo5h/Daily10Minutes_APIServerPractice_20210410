a39b576e23a5ed7056daa9a009f067fd6d680b50
전은형 <ooooo5h91@gmail.com>
2021-04-10 15:59:26 +0900

    토큰값 저장/조회 기능 추가  - 조회시에는 절대 null이 나오지 않도록 조치

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/utils/ContextUtil.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/utils/ContextUtil.kt
@@ -10,6 +10,8 @@ class ContextUtil {
 
         private val AUTO_LOGIN = "AUTO_LOGIN"
 
+        private val LOGIN_TOKEN = "LOGIN_TOKEN"
+
 //        자동 로그인 설정여부 저장(setter) 함수
 
         fun setAutoLogin(context : Context, autoLogin : Boolean) {
@@ -32,6 +34,23 @@ class ContextUtil {
             return pref.getBoolean(AUTO_LOGIN, false)
 
         }
+
+        fun setLoginToken(context: Context, token : String) {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+             pref.edit().putString(LOGIN_TOKEN, token).apply()
+
+        }
+
+        fun getLoginToken(context: Context) : String {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getString(LOGIN_TOKEN, "")!!
+
+
+
+        }
+
+
 
 
 
