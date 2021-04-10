61dac3fa9bf1dd11216ca6d206b27260940def33
전은형 <ooooo5h91@gmail.com>
2021-04-10 12:08:15 +0900

    로그인 API 호출 기능 작성

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/utils/ServerUtil.kt
@@ -1,5 +1,10 @@
 package com.neppplus.daily10minutes_apiserverpractice_20210410.utils
 
+import okhttp3.FormBody
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class ServerUtil {
 
     companion object {
@@ -13,6 +18,33 @@ class ServerUtil {
 //        서버에 로그인을 요청하는 기능 => 함수로 만들고 사용하자
 
         fun postRequestLogin(email : String, pw : String) {
+
+//            어느 주소로 가야하나? 호스트주소/기능주소
+//            ex. 로그인 => http://15.164.153.174  HOST/user => 최종 주소 완성
+
+            val urlString = "${HOST_URL}/user"
+
+//            갈때 어떤 파라미터를 가져가야하나? POST vs GET 에 따라 다르다.
+//            POST - formData에 데이터 첨부
+
+            val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", pw)
+                .build()
+
+//            모든 정보를 종합하고 갈때 어떤 메쏘드로 가느냐?
+
+            val request = Request.Builder()
+                .url(urlString)   // 어디로 가는지?
+                .post(formData)   // POST 방식 - 필요 데이터 (formData) 들고 가도록
+                .build()
+
+//            정리된 정보를 들고 => 실제로 API 요청 진행
+
+//            클라이언트로써 동작하는 코드를 쉽게 작성하도록 도와주는 라이브러리 : OkHttp
+            val client = OkHttpClient()
+
+            client.newCall(request)
 
 
 
