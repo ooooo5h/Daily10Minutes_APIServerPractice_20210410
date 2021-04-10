cc99c6cb74bedd30e1d2c70f072552fc7c183409
전은형 <ooooo5h91@gmail.com>
2021-04-10 14:04:29 +0900

    회원가입 화면 추가

---
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/SignUpActivity.kt
@@ -0,0 +1,24 @@
+package com.neppplus.daily10minutes_apiserverpractice_20210410
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
