038d3a858d2fb2e6aede484cfe3213f49702a1cc
전은형 <ooooo5h91@gmail.com>
2021-04-10 14:35:50 +0900

    회원가입을 실제로 실행하고나서 서버의 응답까지 대응해줬음

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/SignUpActivity.kt
@@ -2,7 +2,10 @@ package com.neppplus.daily10minutes_apiserverpractice_20210410
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.neppplus.daily10minutes_apiserverpractice_20210410.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -20,6 +23,34 @@ class SignUpActivity : BaseActivity() {
             val inputEmail = emailEdt.text.toString()
             val inputPw = passwordEdt.text.toString()
             val inputNickname = nicknameEdt.text.toString()
+
+            ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+
+                        runOnUiThread {
+
+                            Toast.makeText(mContext, "회원가입을 환영합니다", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+
+                    }
+                    else {
+                        val message = jsonObj.getString("message")
+
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+
+                }
+
+
+            })
 
 
 
