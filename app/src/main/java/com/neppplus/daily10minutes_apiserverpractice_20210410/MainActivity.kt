e5486da41e0c35be5177b931f802f64aae17b5a1
전은형 <ooooo5h91@gmail.com>
2021-04-17 11:29:18 +0900

    프로젝트 목록 받아오는 기능 추가  - 메인화면에서 사용할 예정

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/MainActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/MainActivity.kt
@@ -2,8 +2,12 @@ package com.neppplus.daily10minutes_apiserverpractice_20210410
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.daily10minutes_apiserverpractice_20210410.datas.Project
+import com.neppplus.daily10minutes_apiserverpractice_20210410.utils.ServerUtil
 
 class MainActivity : BaseActivity() {
+
+    val mProject = ArrayList<Project>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,16 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        서버에서 => 보여줄만한 프로젝트 목록이 어떤 것들이 있는 지 받아서 Project() 형태로 변환해서 mProjects 에 하나하나 추가해주자
+        getProjectListFromServer()
+
+
+    }
+
+    fun getProjectListFromServer() {
+//        실제로 서버에서 받아오는 기능만 수행
+
+//        ServerUtil.
     }
 
 
