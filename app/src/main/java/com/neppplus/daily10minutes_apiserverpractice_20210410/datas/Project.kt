912655c1c5532b6f8de573df69bff221ae203f57
전은형 <ooooo5h91@gmail.com>
2021-04-17 14:12:26 +0900

    인증 방법 받아서 파싱하고 저장

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/datas/Project.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/datas/Project.kt
@@ -8,11 +8,12 @@ class Project(
         var title : String,
         var imageUrl : String,
         var description : String,
-        var onGoingUserCount : Int) : Serializable {
+        var onGoingUserCount : Int,
+        var proofMethod : String) : Serializable {
 
 //    보조 생성자 추가 => 왜? Project() 만으로도 만들 수 있게 하기 위해서
 
-    constructor() : this(0, "", "", "", 0)
+    constructor() : this(0, "", "", "", 0, "")
 
 //    기능을 추가 => JSON에 넣으면(input) : 알아서 Project로 변환(return)해주는 기능을 구현하자 => 단순 기능은 companion object를 이용하는게 편하다
 
@@ -29,6 +30,8 @@ class Project(
             project.description = jsonObj.getString("description")
 
             project.onGoingUserCount = jsonObj.getInt("ongoing_users_count")
+
+            project.proofMethod = jsonObj.getString("proof_method")
 
 //            완성된 project가 결과로 나가도록
             return project
