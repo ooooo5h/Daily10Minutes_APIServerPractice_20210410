43fed782359a3ff527da114bdff4a866e341c0b3
전은형 <ooooo5h91@gmail.com>
2021-04-17 10:50:40 +0900

    프로젝트 정보 표기용 클래스 추가  - 서버 데이터 참고해서 작업

---
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/datas/Project.kt
@@ -0,0 +1,8 @@
+package com.neppplus.daily10minutes_apiserverpractice_20210410.datas
+
+class Project(
+    var id : Int,
+    var title : String,
+    var imageUrl : String,
+    var description : String) {
+}
