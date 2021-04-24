308c24e4adb7929735a59e7d8af220bee8a460ef
전은형 <ooooo5h91@gmail.com>
2021-04-24 14:11:57 +0900

    커스텀 액션바 xml만 간단히 추가

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/BaseActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/BaseActivity.kt
@@ -8,4 +8,16 @@ abstract class BaseActivity : AppCompatActivity() {
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+//    커스텀 액션바를 적용해주는 기능 (함수)
+
+    fun setCustomActionBar(){
+
+//        1. 액션바가 어떤 모양으로 보이게 하고 싶은지?
+//        => 모양 : layout - xml 작성하자
+
+    }
+
+
+
 }
