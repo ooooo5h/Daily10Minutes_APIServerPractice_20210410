73ab45539407fbcdcce959a8e6c3fc26181684a3
전은형 <ooooo5h91@gmail.com>
2021-04-24 10:06:22 +0900

    인증글 상세보기 화면 작업중

---
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/ViewProofByDateActivity.kt
@@ -0,0 +1,13 @@
+package com.neppplus.daily10minutes_apiserverpractice_20210410
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProofByDateActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_proof_by_date)
+    }
+}
