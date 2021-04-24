287ad3b57f5ceae4bebdf3aeeb0b5b4a50129831
전은형 <ooooo5h91@gmail.com>
2021-04-24 10:49:09 +0900

    SimpleDateFormat 활용 체험

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/ViewProofByDateActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/ViewProofByDateActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.DatePicker
 import kotlinx.android.synthetic.main.activity_view_proof_by_date.*
+import java.text.SimpleDateFormat
 import java.time.Year
 import java.util.*
 
@@ -31,6 +32,14 @@ class ViewProofByDateActivity : BaseActivity() {
 //                    선택된 년/월/일이 변수에 담겨있다 이걸 텍스트뷰에 반영해주자
 
                     Log.d("선택된 날짜", "${year}년 ${month}월 ${dayOfMonth}일")
+
+//                    선택일자를 멤버변수에 저장
+
+                    mSelectedDate.set(year, month, dayOfMonth)
+
+//                    SimpleDateFormat 이용, 날짜를 String 양식으로 가공
+                    val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd")
+                    dateTxt.text = simpleDateFormat.format(mSelectedDate.time)
 
 
                 }
